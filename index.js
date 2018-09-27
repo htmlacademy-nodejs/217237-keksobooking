ca7e63c080b8b737f08eff22357cbb2051c4f14f
b.medvedev <b.medvedev@icloud.com>
2018-09-27 22:14:52 +0300

    add settings of project

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
-const {version, name, author} = require('./package.json');
+const {version, name, author} = require(`./package.json`);
 
-const getMessage = command => {
+const getMessage = (command) => {
   const messages = {
     '--version': version,
-    '--help': `
-      Доступные команды:
-      --help — печатает этот текст;
-      --version — печатает версию приложения;
-    `
+    '--help': `Доступные команды:\n--help — печатает этот текст;\n--version — печатает версию приложения;`
   };
 
-  if (!command) return {
-    message: `
-      Привет пользователь!
-      Эта программа будет запускать сервер ${name}.
-      Автор: ${author}
-    `,
-    code: 0
-  };
+  if (!command) {
+    return {
+      message: `Привет пользователь!\nЭта программа будет запускать сервер ${name}.\nАвтор: ${author}`,
+      code: 0
+    };
+  }
 
-  if (!messages.hasOwnProperty(command)) return {
-    message: `
-      Неизвестная команда ${command}.
-      Чтобы прочитать правила использования приложения, наберите "--help"
-    `,
-    code: 1
-  };
+  if (!messages.hasOwnProperty(command)) {
+    return {
+      message: `Неизвестная команда ${command}.\nЧтобы прочитать правила использования приложения, наберите "--help"`,
+      code: 1
+    };
+  }
 
   return {message: messages[command], code: 0};
 };
